464315d44b89275a07288fa5dc56c68d2c14e6b3
dv-joan <joan300501@gmail.com>
2023-04-30 10:31:32 -0500

    version: Realease form DAW UNCP Course

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
+	return (
+		<div className="App">
+			<header className="flex flex-col justify-center lg:justify-between  lg:mx-10">
+				<div className="my-3 ml-24 lg:ml-0">
+					<img
+						src="https://wp.ditsolution.net/techno/wp-content/uploads/2021/02/main-logo.png"
+						alt="main-logo"
+						width={170}
+					/>
+				</div>
+				<nav className="bg-[#0C5ADB]">
+					<div className="py-3 pl-3">
+						<span className="text-white">TECHNO</span>
+					</div>
+				</nav>
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+				<nav className="invisible lg:visible flex gap-2 text-red-400">
+					<ul>
+						<li>Homee</li>
+						<li>Company</li>
+						<li>Services</li>
+						<li>Pages</li>
+						<li>Elements</li>
+						<li>Blog</li>
+						<li>Contact</li>
+						<li>
+							<img src="" alt="icon1" />
+						</li>
+						<li>
+							<img src="" alt="icon2" />
+						</li>
+					</ul>
+				</nav>
+			</header>
+			<Home />
+		</div>
+	);
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+
+function Home() {
+	return (
+		<section id="home">
+			<div className="absloute">
+				<img
+					src="https://wp.ditsolution.net/techno/wp-content/uploads/2020/10/blog4.jpg"
+					alt="background"
+				/>
+
+				<div className="relative bottom-48">
+					<div>
+						<h3 className="text-2xl tracking-wider uppercase text-white">
+							Total IT Solution
+						</h3>
+						<h1 className="text-4xl text-white font-bold">
+							Best IT solution agency <br />
+							for your business
+						</h1>
+					</div>
+					<div className="flex gap-4">
+						<button
+							className="py-3 px-7 text-white rounded bg-[#0C5ADB] hover:bg-[#074ab4] duration-300"
+							onClick={() => alert("How IT Works")}
+						>
+							How IT works
+						</button>
+						<button
+							className="py-3 px-7 text-white border-[1px] bg-transparent hover:bg-[#0C5ADB] hover:border-[#0C5ADB]  duration-500 border-neutral-200"
+							onClick={() => alert("IT Services")}
+						>
+							IT Services
+						</button>
+					</div>
+				</div>
+				<div className="lg:flex-row flex-col flex gap-2 ">
+					<button onClick={() => alert("Play")}>Play</button>
+				</div>
+			</div>
+			<div className="lg:flex-row flex-col flex gap-2 ">
+				<div
+					id="card1"
+					className="shadow-lg rounded text-center flex-col flex justify-center items-center hover:bg-blue-600 duration-200 hover:text-white"
+				>
+					<img src="" alt="icon" />
+					<span> Provide All King Of IT Services </span>
+					<span id="hover-text">
+						Provide All King Of IT Services
+						<h5 className="invisible duration-100 hover:visible">
+							Whether bringing new and amazing products and on a service to
+							market
+						</h5>
+					</span>
+				</div>
+				<div id="card2">
+					<img src="" alt="icon" />
+					<span> Solution For All IT Security </span>
+					<span id="hover-text">
+						Provide All King Of IT Services
+						<h5>
+							Whether bringing new and amazing products and on a service to
+							market
+						</h5>
+					</span>
+				</div>
+				<div id="card3">
+					<img src="" alt="icon" />
+					<span>All IT Consultancy Solution </span>
+					<span id="hover-text">
+						Provide All King Of IT Services
+						<h5>
+							Whether bringing new and amazing products and on a service to
+							market
+						</h5>
+					</span>
+				</div>
+				<div id="card3">
+					<img src="" alt="icon" />
+					<span>Best Experience Engineer </span>
+					<span id="hover-text">
+						Provide All King Of IT Services
+						<h5>
+							Whether bringing new and amazing products and on a service to
+							market
+						</h5>
+					</span>
+				</div>
+			</div>
+		</section>
+	);
+}
+
+export default Home;
